d1d87a00a2e3f0e0d57676da07ccefb7075df07a
Sharda <53011896+shardapatil@users.noreply.github.com>
2023-06-30 22:38:21 +0530

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EmployeeDemoProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
